8bd1ce99a27129a1c79918bdb23e20ff6e3a4766
Andrew Halls <andrew@galtsoft.com>
2017-04-01 07:02:09 -0600

    Fix for memory issues with mapbox

--- app/map.component.ts
+++ app/map.component.ts
@@ -1,25 +1,51 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, } from "@angular/core";
 import {RouterExtensions} from "nativescript-angular/router";
 import { GC } from 'utils/utils';
+import { isIOS } from 'platform';
 
 @Component({
     selector: "map",
     templateUrl: "map.component.html",
 })
-export class MapComponent {
+export class MapComponent implements OnDestroy {
 
    constructor(
         private _routerExtensions: RouterExtensions
     ) { }
+    private mapbox;
+    @ViewChild("mapboxview") mapboxViewRef: ElementRef;
 
+    ngOnInit() {
+        console.log("GroundComponent::ngOnInit");
+        GC();
+    }
+
+   ngOnDestroy() {
+       console.log("MapComponent::ngOnDestroy");
+       //console.dump(this.mapboxViewRef.nativeElement);
+       // this.mapbox.destroy();
+       if (isIOS) {
+                 this.mapbox.mapView.removeFromSuperview();
+                 this.mapbox.mapView.delegate = null;
+                 this.mapbox.mapView = null;
+       }
+       setTimeout(() => this.cleanupAfterDelay(), 500)
+        
+    }
+    cleanupAfterDelay() {
+        console.log("MapComponent::cleanupAfterDelay:GC");
+        GC();
+    }
     public onBackTap() {
         this._routerExtensions.navigate(["/tap"], 
             <any>  { clearHistory: true });
         GC();
-        console.log("onBackTap:GC()");
+        console.log("MapComponent::onBackTap:GC");
     }
     
     public onMapReady(args) {
-
+        this.mapbox = args.map;
+        console.log("MapComponent::onMapReady")
+        console.dump(this.mapbox)
     }
 }
